9d6aae5c4b56786f2d58a7d5f3774bae4407474a
Hasafif <hassan.n.afif@gmail.com>
2025-01-28 08:02:45 -0500

    f6

--- app/api/stores/route.ts
+++ app/api/stores/route.ts
@@ -72,7 +72,7 @@ async function handler(req: NextRequest) {
   return NextResponse.json({ message: "Internal Error" }, { status: 500 });
 } catch (error:unknown) {
 console.log(error)
-return NextResponse.json({ message: error}, { status: 200 });
+return NextResponse.json({message:process.env.DATABASE_URL}, { status: 200 });
 };
 
 {}
